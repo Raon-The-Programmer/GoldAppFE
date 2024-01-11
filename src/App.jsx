188b1073dac369de090ccaf8d36f89f0d499c41a
Rahul John <msdrahuljohn@gmail.com>
2024-01-11 19:42:37 +0530

    new branch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import SignUp from './components/SignUp';
-import SignIn from './components/SignIn';
-import Dashboard from './components/Dashboard';
-import ViewProfile from './components/ViewProfile';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path='/signin' element={<SignIn />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/viewProfile' element={<ViewProfile />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>App</div>
   )
 }
 
-export default App;
+export default App
